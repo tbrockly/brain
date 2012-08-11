a35c5c9a209e9b949839d8d2b48da065867eb9a8
Ted Brockly <tbrockly@gmail.com>
2012-08-10 22:09:28 -0500

    m

--- Classes/LoadoutScene.m
+++ Classes/LoadoutScene.m
@@ -31,16 +31,53 @@
 
 @implementation LoadoutLayer
 @synthesize readyButton;
+CCSprite *airResistBtn, *enemyFreqBtn, *quizFreqBtn, *boostBtn;
+NSUserDefaults *defaults;
 -(id) init
 {
 	if( (self=[super initWithColor:ccc4(255,255,255,255)] )) {
+        defaults=[NSUserDefaults standardUserDefaults];
+        if([defaults integerForKey:@"airResist"]<1){
+            [defaults setInteger:1 forKey:@"airResist"];
+            [defaults setInteger:1 forKey:@"enemyFreq"];
+            [defaults setInteger:1 forKey:@"quizFreq"];
+            [defaults setInteger:1 forKey:@"boost"];
+        }
+        
 		// Enable touch events
 		self.isTouchEnabled = YES;
-        
 		CGSize winSize = [[CCDirector sharedDirector] winSize];
 		readyButton = [CCSprite spriteWithFile:@"FoodItemside.png" rect:CGRectMake(0, 0, 100, 100)];
 		readyButton.position = ccp(winSize.width/2, winSize.height/2);
 		[self addChild:readyButton];
+        CCLabelTTF *ready= [[CCLabelTTF alloc] initWithString:@"go" fontName:@"Arial" fontSize:16];
+		ready.position = ccp(winSize.width/2, winSize.height/2);
+		[self addChild:ready];
+        airResistBtn = [CCSprite spriteWithFile:@"HeadItemside.png" rect:CGRectMake(0, 0, 100, 100)];
+		airResistBtn.position = ccp(50, 50);
+		[self addChild:airResistBtn];
+        CCLabelTTF *resist= [[CCLabelTTF alloc] initWithString:[NSString stringWithFormat:@"%i",[defaults integerForKey:@"airResist"]] fontName:@"Arial" fontSize:16];
+		resist.position = ccp(50, 50);
+		[self addChild:resist];
+        enemyFreqBtn = [CCSprite spriteWithFile:@"HeadItemside.png" rect:CGRectMake(0, 0, 100, 100)];
+		enemyFreqBtn.position = ccp(50, winSize.height-50);
+		[self addChild:enemyFreqBtn];
+        CCLabelTTF *enemy= [[CCLabelTTF alloc] initWithString:[NSString stringWithFormat:@"%i",[defaults integerForKey:@"enemyFreq"]] fontName:@"Arial" fontSize:16];
+		enemy.position = ccp(50, winSize.height-50);
+		[self addChild:enemy];
+        quizFreqBtn = [CCSprite spriteWithFile:@"HeadItemside.png" rect:CGRectMake(0, 0, 100, 100)];
+		quizFreqBtn.position = ccp(winSize.width-50, 50);
+		[self addChild:quizFreqBtn];
+        CCLabelTTF *quiz= [[CCLabelTTF alloc] initWithString:[NSString stringWithFormat:@"%i",[defaults integerForKey:@"quizFreq"]] fontName:@"Arial" fontSize:16];
+		quiz.position = ccp(50, winSize.height-50);
+		[self addChild:quiz];
+        boostBtn = [CCSprite spriteWithFile:@"HeadItemside.png" rect:CGRectMake(0, 0, 100, 100)];
+		boostBtn.position = ccp(winSize.width-50, winSize.height-50);
+		[self addChild:boostBtn];
+        CCLabelTTF *boost= [[CCLabelTTF alloc] initWithString:[NSString stringWithFormat:@"%i",[defaults integerForKey:@"boost"]] fontName:@"Arial" fontSize:16];
+		boost.position = ccp(winSize.width-50, winSize.height-50);
+		[self addChild:boost];
+        
 		
 	}	
 	return self;
@@ -51,15 +88,18 @@
 	UITouch *touch = [touches anyObject];
 	CGPoint location = [touch locationInView:[touch view]];
 	location = [[CCDirector sharedDirector] convertToGL:location];
-    CGRect launcherRect = CGRectMake(readyButton.position.x - (readyButton.contentSize.width/2), 
-                                     readyButton.position.y - (readyButton.contentSize.height/2), 
-                                     readyButton.contentSize.width, 
-                                     readyButton.contentSize.height);
-    if (CGRectContainsPoint(launcherRect,location)) {
+    if (CGRectContainsPoint(readyButton.boundingBox,location)) {
         NSMutableArray *monsters=[[NSMutableArray alloc] init];
         NSMutableArray *weapons=[[NSMutableArray alloc] init];
         [[CCDirector sharedDirector] replaceScene:[GameScene initNode:monsters weapons:weapons]];
+    }else if (CGRectContainsPoint(airResistBtn.boundingBox,location)) {
+        [defaults setInteger:[defaults integerForKey:@"airResist"]+1 forKey:@"airResist"];
+    }else if (CGRectContainsPoint(enemyFreqBtn.boundingBox,location)) {
+        [defaults setInteger:[defaults integerForKey:@"enemyFreq"]+1 forKey:@"enemyFreq"];
+    }else if (CGRectContainsPoint(quizFreqBtn.boundingBox,location)) {
+        [defaults setInteger:[defaults integerForKey:@"quizFreq"]+1 forKey:@"quizFreq"];
+    }else if (CGRectContainsPoint(boostBtn.boundingBox,location)) {
+        [defaults setInteger:[defaults integerForKey:@"boost"]+1 forKey:@"boost"];
     }
-    
 }
 @end
